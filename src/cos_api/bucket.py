938b61a61ae97f33b18bdea7cbdf2929f88cca0a
hx-w <herixth@163.com>
2022-04-11 09:56:50 +0800

    change env prefix

--- src/cos_api/bucket.py
+++ src/cos_api/bucket.py
@@ -7,12 +7,12 @@ from tqdm import tqdm
 
 class BucketAPI(object):
     def __init__(self):
-        _secret_id = os.getenv('secrets.TENCENTCLOUDSECRETID')
-        _secret_key = os.getenv('secrets.TENCENTCLOUDSECRETKEY')
+        _secret_id = os.getenv('TencentCloud.TENCENTCLOUDSECRETID')
+        _secret_key = os.getenv('TencentCloud.TENCENTCLOUDSECRETKEY')
         _region = 'ap-chengdu'
         _config = CosConfig(Region=_region, SecretId=_secret_id, SecretKey=_secret_key)
         self.cos_client = CosS3Client(_config)
-        self.bucket_name = os.getenv('secrets.TENCENTCOUDBUCKETNAME')
+        self.bucket_name = os.getenv('TencentCloud.TENCENTCOUDBUCKETNAME')
 
     def list_matches(self, mapname: str) -> list: # 'de_inferno/123123/'
         response = self.cos_client.list_objects(
